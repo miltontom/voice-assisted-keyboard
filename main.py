d675c690c58776ef4b3b908599859ce1f8a93cda
Milton Tom <miltontom16@gmail.com>
2025-01-26 06:31:23 +0530

    add basic speech recognition

---
+++ main.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as mic:
+        print("Listening...")
+        audio = recognizer.listen(mic)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print(text)
+    except sr.UnknownValueError:
+        print("Sorry, couldn't get that...")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google speech recognition service {e}")
+
+
+def main() -> None:
+    speech_to_text()
+
+
+if __name__ == "__main__":
+    main()
